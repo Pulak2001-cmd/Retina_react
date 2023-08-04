2ca3de959f4d1f57a4d31bc5d104a6021bd2f745
Pulak <pulakkumarghosh2001@gmail.com>
2023-08-04 18:24:34 +0530

    Patient Data

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -19,6 +19,9 @@ export default function Patient() {
     setIds([109, 321, 1501, 2687, 7024, 10728, 11282, 14641, 15485, 17991])
   }, [])
   const submit = ()=> {
+    if(parseInt(model) === 1){
+        
+    }
     setBody(false);
     setResult2(true);
   }
@@ -41,6 +44,38 @@ export default function Patient() {
         })
         console.log(dicts)
         setDates(arr);
+    } else if(parseInt(ml) === 1) {
+        console.log("hiiii")
+        let arr = [];
+        let dicts = {};
+        let files = []
+        await db.where('patient_id', '==', currId.toString()).get().then(async (query) => {
+            const list = query.docs;
+            for(let i = 0; i < list.length; i++) {
+                let d = list[i].data();
+                if(files.includes(d.filename)){
+                    continue;
+                }
+                files.push(d.filename)
+                let temp = {
+                    date: d.date,
+                    category: d.data.category_class,
+                    category_score: d.data.category_score,
+                    filename: d.filename,
+                    url: d.url
+                }
+                if(dicts[d.date]){
+                    if(!dicts[d.date].includes(temp)){
+                        dicts[d.date].push(temp);
+                    }
+                } else {
+                    dicts[d.date] = [temp];
+                }
+                arr.push(temp);
+            }
+            console.log('dicts:', dicts);
+        })
+        setModel1Data(dicts);
     }
   }
   const setChange = async (e) => {
@@ -62,10 +97,54 @@ export default function Patient() {
   const [selectDate, setSelectDate] = useState('');
   const [result2, setResult2] = useState(false);
   const [dateData, setDateData] = useState([]);
+  const [model1Data, setModel1Data] = useState({});
   return (
     <div className="body1">
         <Navbar />
-        {result2 &&
+        {parseInt(model) === 1 && result2 && 
+            <div className="p-3 d-flex flex-column align-items-center justify-conter-center">
+                <button className="btn btn-primary" onClick={()=> {
+                    setBody(true);
+                    setResult2(false);
+                    setCurrId('');
+                    setModel(0);
+                    setSelectDate('');
+                }}>Upload Again</button>
+                <h1 class="p-4 mt-4" style={{backgroundColor: 'rgb(0, 0, 0, 0.5)', fontSize: '70px', borderRadius: '15px', color: 'white'}}>Result</h1>
+                <h3 class="p-2 mt-1" style={{backgroundColor: 'rgb(0, 0, 0, 0.5)', fontSize: '40px', borderRadius: '15px', color: 'white'}}>Patient ID : {currId}</h3>
+                {Object.keys(model1Data).map((item, index) =>(
+                    <div key={index}>
+                    <div class="dateBackground">
+                    <h1 class="text-light">Date : {item} </h1>
+                </div>
+                <div class="imgBack">
+                    
+                    {model1Data[item].map((i, index)=> (
+                        <div class='element' key={index}>
+                            <h5 class="text-light">Uploaded Image </h5>
+                            <p>Filename : {i.filename}</p>
+                            <img src={i.url} class="bg-light p-1 m-2" height="200" width="205" alt="image" />
+                            <h4 class="text-light">Retinal Image Analysis</h4>
+                            <p> DR Grade: {i.category} ({i.category_score}) </p> 
+                            <div class="d-flex flex-row">
+                                <p class="m-0 marker1">0</p>
+                                <p class="m-0 marker1">1</p>
+                                <p class="m-0 marker1">2</p>
+                                <p class="m-0 marker1">3</p>
+                                <p class="m-0 marker1">4</p>
+                                <p class="m-0">5</p>
+                            </div>
+                            <div class="progress2">
+                                <div class="bar7" style={{width: `${200-40*i.category_score}px`}}></div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                </div>
+                ))}
+            </div>
+        }
+        { parseInt(model) === 2 && result2 &&
             <div className="p-3 d-flex flex-column align-items-center justify-conter-center">
                 <h1 class="p-4 mt-4" style={{backgroundColor: 'rgb(0, 0, 0, 0.5)', fontSize: '70px', borderRadius: '15px', color: 'white'}}>Result</h1>
             <div className="p-2 m-2 d-flex flex-column align-items-center justify-content-center" style={{backgroundColor: 'rgb(0,0,0,0.8)', borderRadius: 10}}>
